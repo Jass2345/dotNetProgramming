47a19ea8566f0385131fc96008fa9f419902e3a3
Jass2345 <wotjr030713@naver.com>
2026-04-08 20:33:00 +0900

    연습0408

---
+++ dotNetPractice/Program.cs
@@ -0,0 +1,21 @@
+﻿//0325 실습문제
+Console.Write("영문 문자열을 입력하세요: ");
+string input = Console.ReadLine().ToLower();
+int count = 0;
+
+foreach (char ch in input)
+{
+    switch(ch)
+    {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+            count++;
+            break;
+        default:
+            break;
+    }
+}
+Console.WriteLine($"모음의 갯수는 {count}개 입니다.");
